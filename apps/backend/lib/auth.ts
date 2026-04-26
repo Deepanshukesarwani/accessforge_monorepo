4018c009d3fae62728213741d2d13ebb886e195e
Deepanshu kesarwani <kesarwanideepanshu428@gmail.com>
2026-04-26 22:28:25 +0530

    feat: enhance authentication configuration with secret and plugins

--- apps/backend/lib/auth.ts
+++ apps/backend/lib/auth.ts
@@ -1,12 +1,15 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { dash } from "@better-auth/infra";
 import { MongoClient } from "mongodb";
 
 const client = new MongoClient(`${process.env.DATABASE_URL}`);
 const db = client.db();
 export const auth = betterAuth({
-   database: mongodbAdapter(db),
-     emailAndPassword: { 
-    enabled: true, 
+  database: mongodbAdapter(db),
+  secret: process.env.BETTER_AUTH_SECRET,
+  emailAndPassword: {
+    enabled: true,
   },
+  plugins: [dash()],
 });
